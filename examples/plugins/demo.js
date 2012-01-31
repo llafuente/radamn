0ef6b50cba1859f830e92156be92311aae919a6b
unknown <bls@bls-PC.(none)>
2012-01-31 19:13:08 +0100

    browser compatible :)

--- examples/plugins/demo.js
+++ examples/plugins/demo.js
@@ -1,9 +1,12 @@
-var fps = require('./fps');
-var grid = require('./grid');
+(function(exports, browser) {
+
+if(!browser) {
+	var fps = require('./fps');
+	var grid = require('./grid');
+}
 
 
-
-module.exports.attachFPSCounter = function(window, xpos, ypos) {
+exports.attachFPSCounter = function(window, xpos, ypos) {
 	xpos = xpos || 0;
 	ypos = ypos || 0;
 	fps = new fps({
@@ -19,7 +22,7 @@ module.exports.attachFPSCounter = function(window, xpos, ypos) {
 }
 
 
-module.exports.attachGrid = function(window, options) {
+exports.attachGrid = function(window, options) {
 	grid = new grid({});
 
 	var gridnode = new Radamn.Node().appendEntity(grid);
@@ -28,7 +31,7 @@ module.exports.attachGrid = function(window, options) {
 }
 
 
-module.exports.attachEscapeInputs = function(window) {
+exports.attachEscapeInputs = function(window) {
 
 	Radamn.addEvent("quit", function(e) {
 		Radamn.quit();
@@ -43,25 +46,30 @@ module.exports.attachEscapeInputs = function(window) {
 	});
 }
 
-module.exports.demoWindow = function(width, height, caption, grid) {
+exports.demoWindow = function(width, height, caption, grid) {
 	grid = grid || false;
 	// visual test
 	console.log("demo/ create window");
 	var win = Radamn.createWindow(640, 480);
 
-	console.log("demo/ display FPS");
-	module.exports.attachFPSCounter(win, 480, 0);
+	if(!browser) {
+		console.log("demo/ display FPS");
+		exports.attachFPSCounter(win, 480, 0);
+	}
 
 	console.log("demo/ attaching ESC/F5/Close window:");
-	module.exports.attachEscapeInputs(win);
+	exports.attachEscapeInputs(win);
 	
 	console.log("demo/ set caption to: "+caption);
 	win.setCaption(caption, caption);
 	
 	if(grid) {
 		console.log("demo/ display grid");
-		module.exports.attachGrid(win, grid);
+		exports.attachGrid(win, grid);
 	}
 	
 	return win;
 }
+
+})(typeof exports === "undefined" ? (this.demo = {}) : exports, typeof exports === "undefined");
+
