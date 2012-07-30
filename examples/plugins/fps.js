9a30dfc4e61bfb2384f1939b4c3e03bc0ac1f34d
llafuente <llafuente@noboxout.com>
2012-07-30 11:02:04 +0200

    run on windows!

--- examples/plugins/fps.js
+++ examples/plugins/fps.js
@@ -1,30 +1,35 @@
-module.exports = new Class({
-    Implements: [Options],
-    /**
-     * @type Font
-     */
-    font: null,
+(function(exports, browser) {
+    var Class = browser ? $.Class : require("node-class").Class,
+        Events = browser ? $.Events : require("node-class").Events,
+        typeOf = browser ? $.typeof : require("node-class").typeof;
 
-    options: {
+    module.exports = new Class("PluginFPS", {
+        /**
+         * @type Font
+         */
+        font: null,
+
         font: "./resources/fonts/Jura-DemiBold.ttf",
         size: 32,
         color: "#00FF00",
         x: 0,
         y: 0
-    },
+    });
 
-    initialize: function(options) {
-        this.setOptions(options);
-        this.font = Radamn.Assets.getFont(this.options.font, this.options.size);
-    },
-    draw: function(ctx, delta) {
-        var fps = Math.round(1000 / delta,1)+"";
-		
-		var old = ctx.fillStyle;
-		ctx.font = this.options.size+"px "+"Jura";
-		ctx.fillStyle = this.options.color;
-		ctx.fillText(fps, this.options.x, this.options.y);
-		
-		ctx.fillStyle = old;
-    }
-});
+    module.exports.implements({
+        __construct: function(options) {
+            this.font = Radamn.Assets.getFont(this.font, this.size);
+        },
+        draw: function(ctx, delta) {
+            var fps = Math.round(1000 / delta,1)+"";
+
+            var old = ctx.fillStyle;
+            ctx.font = this.size+"px "+"Jura";
+            ctx.fillStyle = this.color;
+            ctx.fillText(fps, this.x, this.y);
+
+            ctx.fillStyle = old;
+        }
+    });
+
+})(typeof exports === "undefined" ? Radamn : exports, typeof exports === "undefined");
--- examples/plugins/grid.js
+++ examples/plugins/grid.js
@@ -1,37 +1,42 @@
-module.exports = new Class({
-    Implements: [Options],
-    options: {
+(function(exports, browser) {
+    var Class = browser ? $.Class : require("node-class").Class,
+        Events = browser ? $.Events : require("node-class").Events,
+        typeOf = browser ? $.typeof : require("node-class").typeof;
+
+    module.exports = new Class("PluginGrid", {
         size: 16,
         color: "#FF0000",
-		width: 0.25
-    },
+        width: 0.25
+    });
 
-    initialize: function(options) {
-        this.setOptions(options);
-    },
-    draw: function(ctx, delta) {
-		var win = ctx.getWindow(),
-			x = Math.floor(win.width / this.options.size),
-			y = Math.floor(win.height / this.options.size);
+    module.exports.implements({
+        __construct: function(options) {
+        },
+        draw: function(ctx, delta) {
+            var win = ctx.getWindow(),
+                x = Math.floor(win.width / this.size),
+                y = Math.floor(win.height / this.size);
 
-		ctx.strokeStyle = this.options.color;
-		ctx.lineWidth = this.options.width;
+            ctx.strokeStyle = this.color;
+            ctx.lineWidth = this.width;
 
-		for(i=0;i<x; ++i) {
-			ctx.translate(this.options.size, 0);
-			ctx.beginPath();
-			ctx.lineTo(0, win.height);
-			ctx.closePath();
-			ctx.stroke();
-		}
-		ctx.translate( -x*this.options.size, 0);
-		for(i=0;i<y; ++i) {
-			ctx.translate(0, this.options.size);
-			ctx.beginPath();
-			ctx.lineTo(win.width, 0);
-			ctx.closePath();
-			ctx.stroke();
-		}
-		ctx.translate(0, -y*this.options.size);
-    }
-});
+            for(i=0;i<x; ++i) {
+                ctx.translate(this.size, 0);
+                ctx.beginPath();
+                ctx.lineTo(0, win.height);
+                ctx.closePath();
+                ctx.stroke();
+            }
+            ctx.translate( -x*this.size, 0);
+            for(i=0;i<y; ++i) {
+                ctx.translate(0, this.size);
+                ctx.beginPath();
+                ctx.lineTo(win.width, 0);
+                ctx.closePath();
+                ctx.stroke();
+            }
+            ctx.translate(0, -y*this.size);
+        }
+    });
+
+})(typeof exports === "undefined" ? Radamn : exports, typeof exports === "undefined");
